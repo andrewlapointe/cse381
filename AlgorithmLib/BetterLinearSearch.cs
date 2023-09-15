ca0fe8c46f5f479c1311a196e754474f8d5cbbfb
andrewlapointe <andrewrlapointe@gmail.com>
2023-09-15 12:22:59 -0600

    added better linear search - passes all tests

--- AlgorithmLib/BetterLinearSearch.cs
+++ AlgorithmLib/BetterLinearSearch.cs
@@ -4,7 +4,14 @@ public static class BetterLinearSearch
 {
     public static int Search(List<IComparable> data, IComparable target)
     {
-        // ADD CODE HERE AND FIX RETURN STATEMENT
-        return -1;
+        for (var i = 0; i < data.Count; i++)
+        {
+            // Console.WriteLine(i);
+            if (data[i].Equals(target)) // The "==" operator is not supported for iComparables. Use the .Equals() instead
+            {
+                return i; // Break the loop when the answer is found.
+            }
+        }
+        return -1; // Fail Condition
     }
 }
