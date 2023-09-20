ad6421441fa4d06f517b49325e12f5fe4a0a0fbd
andrewlapointe <andrewrlapointe@gmail.com>
2023-09-20 12:28:13 -0600

    binary search algorithm

--- AlgorithmLib/BetterLinearSearch.cs
+++ AlgorithmLib/BetterLinearSearch.cs
@@ -6,12 +6,11 @@ public static class BetterLinearSearch
     {
         for (var i = 0; i < data.Count; i++)
         {
-            // Console.WriteLine(i);
             if (data[i].Equals(target)) // The "==" operator is not supported for iComparables. Use the .Equals() instead
             {
                 return i; // Break the loop when the answer is found.
             }
         }
-        return -1; // Fail Condition
+        return -1; // Fail Condition 
     }
 }
--- AlgorithmLib/BinarySearch.cs
+++ AlgorithmLib/BinarySearch.cs
@@ -1,10 +1,35 @@
-﻿namespace AlgorithmLib;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlgorithmLib;
 
 public static class BinarySearch
 {
     public static int Search(List<IComparable> data, IComparable target)
     {
-        // ADD CODE HERE AND FIX RETURN STATEMENT
-        return -1;
+        int first = 0;
+        int last = data.Count - 1;
+        int index = last / 2;
+
+        while (true)  // Runs until number is found or list is exhausted
+        {
+            if (target.Equals(data[index]))  // Found it!
+            {
+                return index;
+            }
+            if (target.CompareTo(data[index]) < 0)  // CASE: Target is less than the middle
+            {
+                last = index - 1;
+                index = last / 2;
+            }
+            else if (target.CompareTo(data[index]) > 0)   // CASE: Targed is greater than the middle
+            {
+                first = index + 1;
+                index = last / 2;
+            }
+            else if (first > last)  // CASE: Target is not in list
+            {
+                return -1;
+            }
+        }
     }
 }
