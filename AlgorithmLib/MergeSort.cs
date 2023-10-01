9d33137d4ad452305a1a82939545bdb0be79f58b
andrewlapointe <andrewrlapointe@gmail.com>
2023-09-30 18:09:01 -0600

    added comments to merge sort

--- AlgorithmLib/MergeSort.cs
+++ AlgorithmLib/MergeSort.cs
@@ -13,37 +13,43 @@ public static class MergeSort
             return;
 
         var mid = (first + last) / 2;
+        // recursively call the _Sort function until the list is broken it lengths of 1
         _Sort(data, first, mid);
         _Sort(data, mid + 1, last);
+        
+        //merge the data back together in order
         Merge(data, first, mid, last);
     }
 
     private static void Merge(IList<IComparable> data, int first, int mid, int last)
     {
+        // sets the "start" of each half of the list. necessary because the sort is in place
         IList<IComparable> sa1 = new List<IComparable>(data).GetRange(first, mid - first + 1);
         IList<IComparable> sa2 = new List<IComparable>(data).GetRange(mid + 1, last - mid);
-
+        
+        // references the start of each half
         var sa1Index = 0;
         var sa2Index = 0;
-            
+        
+        // reassembles list in order    
         for (var mIndex = first; mIndex <= last; mIndex++)
         {
-            if (sa1Index >= sa1.Count)
+            if (sa1Index >= sa1.Count) // end of first half
             {
                 data[mIndex] = sa2[sa2Index];
                 sa2Index++;
             }
-            else if (sa2Index >= sa2.Count)
+            else if (sa2Index >= sa2.Count) // end of second half
             {
                 data[mIndex] = sa1[sa1Index];
                 sa1Index++;
             }
-            else if (sa1[sa1Index].CompareTo(sa2[sa2Index]) < 0)
+            else if (sa1[sa1Index].CompareTo(sa2[sa2Index]) < 0) // next in first half is greater
             {
                 data[mIndex] = sa1[sa1Index];
                 sa1Index++;
             }
-            else
+            else // next in second half is greater
             {
                 data[mIndex] = sa2[sa2Index];
                 sa2Index++;
